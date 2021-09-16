2e0f15836163badafad63afa050cc6f475061bb3
Julian Nadeau <julian@jnadeau.ca>
2021-09-16 11:30:24 -0400

    Add some additional tests

--- test/controllers/memberships_controller_test.rb
+++ test/controllers/memberships_controller_test.rb
@@ -2,7 +2,32 @@ require "test_helper"
 
 class MembershipsControllerTest < ActionDispatch::IntegrationTest
   include Devise::Test::IntegrationHelpers
-  # test "the truth" do
-  #   assert true
-  # end
+  
+  setup do
+    @membership = memberships(:one)
+    @user = @membership.user
+    @channel = @membership.channel
+    sign_in(@user)
+  end
+
+  test "destroy discards the membership" do
+    assert_no_difference("Membership.count") do
+      assert_difference('Membership.kept.count', -1) do
+        delete channel_membership_url(@channel, @membership)
+      end
+      assert_redirected_to channels_url
+      follow_redirect!
+    end
+  end
+
+  test "destroy redirects to sign in with not logged in" do
+    sign_out(@user)
+
+    assert_no_difference("Membership.count") do
+      assert_no_difference('Membership.kept.count') do
+        delete channel_membership_url(@channel, @membership)
+      end
+      assert_redirected_to new_user_session_path
+    end
+  end
 end
--- test/models/channel_test.rb
+++ test/models/channel_test.rb
@@ -1,7 +1,19 @@
 require "test_helper"
 
 class ChannelTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @channel = channels(:one)
+  end
+
+  test "should return true if the channel has the given member" do
+    member = @channel.members.first
+    refute_nil member
+    assert @channel.member?(member)
+  end
+
+  test "should return false if the channel does not have the given member" do
+    member = User.where.not(id: @channel.members.pluck(:id)).first
+    refute_nil member
+    refute @channel.member?(member)
+  end
 end
